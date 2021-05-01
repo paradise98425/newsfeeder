b23ef418a39ab3a1095b25b3883af41a29fcd8e3
Roshan Pratap Katel <paradise98425@gmail.com>
2021-05-01 17:57:23 +0545

    newsfeed package added

--- httpd/main.go
+++ httpd/main.go
@@ -1,12 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
-import "newsfeeder/httpd/handler"
+import (
+	"newsfeeder/httpd/handler"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 
-	r.GET("/ping", handler.PingGet)
+	r.GET("/ping", handler.PingGet())
 
 	r.Run(":8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
